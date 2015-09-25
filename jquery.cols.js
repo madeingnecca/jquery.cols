432221a1510a4d8f28b899b489914e0e0d0d738c
Damiano Seno <dax86it@gmail.com>
2015-09-25 23:56:09 +0200

    Create jquery.cols.js

---
+++ jquery.cols.js
@@ -0,0 +1,45 @@
+(function($) {
+	'use strict';
+
+	function reorder(table, colCount) {
+		var $table;
+		var $cells;
+		var $cell;
+		var $trs;
+		var $newRow;
+		var cellsCount;
+		var row, col;
+		var next, prev;
+
+		$table = $(table);
+		$trs = $table.find('tr');
+		$cells = $table.find('td');
+		cellsCount = $cells.length;
+
+		prev = {colCount: (cellsCount / $trs.length), rowCount: $trs.length};
+		next = {colCount: colCount, rowCount: (cellsCount / colCount)};
+
+		if (next.rowCount > prev.rowCount) {
+			for (row = 0; row < next.rowCount; row++) {
+				$newRow = $('<tr></tr>');
+				$trs.last().after($newRow);
+				$trs = $trs.add($newRow);
+			}
+		}
+
+		for (row = 0; row < next.rowCount; row++) {
+			for (col = 0; col < next.colCount; col++) {
+				$cell = $cells.eq(row * next.colCount + col);
+				$cell.appendTo($trs.get(row));
+			}
+		}
+
+		$trs.filter(':empty').remove();
+	}
+
+	$.fn.cols = function(colCount) {
+		return $(this).each(function() {
+			reorder(this, colCount);
+		});
+	};
+}(jQuery));
